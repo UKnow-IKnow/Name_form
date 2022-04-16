4cab3633c47d39f83fe2910832910c5d6805e2d3
UKnow-IKnow <sarojsahu014@gmail.com>
2022-04-16 07:54:18 +0530

    Done and dusted

--- App.js
+++ App.js
@@ -1,10 +1,9 @@
-var form = document.getElementById("googleSheetdb");
-form.addEventListener("submit", e => {
-    e.preventDefault();
-    fetch(form.action, {
-        method: "POST" ,
-        body: new FormData(document.getElementById("googleSheetdb")),
-    }).then(
-        Response => Response.json()
-    )
+const scriptURL = 'https://script.google.com/macros/s/AKfycbySgmUea1F4MIoMKl7bbe6viJZhSol9K8vMF6L9zIf4z2rPjMYGqy-qldwy4JObwmzEPQ/exec'
+const form = document.forms['googleSheetdb']
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => alert("Your Name Saved Successfully"))
+    .catch(error => console.error('Error!', error.message))
 })
